5d4b00c0de405f619c8d314f4c3d358e67152419
outfrost <kotlet.bahn@gmail.com>
2018-09-24 15:38:14 +0200

    Add unity3d/Outfrost/InputDispatcher.cs

---
+++ unity3d/Outfrost/InputDispatcher.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Outfrost {
+	
+	public class InputDispatcher : MonoBehaviour {
+		
+		public delegate void KeyDownHandler(KeyCode keyCode);
+		
+		private readonly Dictionary<KeyCode, LinkedList<KeyDownHandler>> keyDownHandlers = new Dictionary<KeyCode, LinkedList<KeyDownHandler>>();
+		
+		private void Update() {
+			// Scan registered key inputs and invoke handlers
+			foreach (var entry in keyDownHandlers) {
+				if (Input.GetKeyDown(entry.Key)) {
+					foreach (KeyDownHandler handler in entry.Value) {
+						handler(entry.Key);
+					}
+				}
+			}
+		}
+		
+		public void AddKeyDownHandler(KeyCode keyCode, KeyDownHandler handler) {
+			LinkedList<KeyDownHandler> list;
+			try {
+				list = new LinkedList<KeyDownHandler>();
+				keyDownHandlers.Add(keyCode, list);
+			}
+			catch (ArgumentException) {
+				list = keyDownHandlers[keyCode];
+			}
+			
+			list.AddLast(handler);
+		}
+		
+	}
+	
+}
